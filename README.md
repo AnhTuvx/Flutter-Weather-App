ce10ee302ebed6967cda25f92c0bc4603ab4a570
AnhTuvx <zzkuxinzz@gmail.com>
2024-10-15 21:47:21 +0700

    finish welcome page

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 import the http lib for making HTTP requests
 1.Create Size variables for Media Queries in class GetStarted
 2.Create a model directory to hold our models
+3.Create a Constants model to hold our constants
+    Replace the colors with our model colors
+4.Create City model to hold our Citys variables and data
+    Create a function to fetch data for selectecd cities
+    Londonis selected by defaults
--- lib/ui/get_started.dart
+++ lib/ui/get_started.dart
@@ -1,17 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:weather_app_flutter/models/constants.dart';
+import 'package:weather_app_flutter/ui/welcome.dart';
 
 class GetStarted extends StatelessWidget {
   const GetStarted({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Constants myConstants = Constants();
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
       body: Container(
         width: size.width,
         height: size.height,
-        color: Colors.red,
+        color: myConstants.primaryColor.withOpacity(.5),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -21,16 +25,25 @@ class GetStarted extends StatelessWidget {
               SizedBox(
                 height: 30,
               ),
-              Container(
-                height: 50,
-                width: size.width * 0.7,
-                decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(10)),
-                child: Center(
-                  child: Text(
-                    "Get Started",
-                    style: TextStyle(fontSize: 18, color: Colors.white),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Welcome(),
+                      ));
+                },
+                child: Container(
+                  height: 50,
+                  width: size.width * 0.7,
+                  decoration: BoxDecoration(
+                      color: myConstants.primaryColor,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Center(
+                    child: Text(
+                      "Get Started",
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
                   ),
                 ),
               ),
